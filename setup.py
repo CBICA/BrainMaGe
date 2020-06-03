9d50eec87c45310aaf8ff0c6628cf04aed68f408
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2020-06-03 09:21:27 -0400

    updated information

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ Created on Sat May 30 01:05:59 2020
 from setuptools import setup
 
 setup(name='Penn-BET',
-      version='1.0',
-      description='Modality Agnostic toolkit for skull stripping',
-      url='https://github.com/Geeks-Sid/Penn-BET',
+      version='1.0.0.Alpha',
+      description='Skull stripping using multiple and single modalities',
+      url='https://github.com/CBICA/Penn-BET',
       python_requires='>=3.6',
       author='Siddhesh Thakur',
       author_email='software@cbica.upenn.edu',
