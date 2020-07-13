d95479dfe7163dded5c8c431a213fba43d3acd29
sarthakpati <sarthak.pati@hotmail.com>
2020-07-13 16:34:14 -0400

    version updated to RC2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 import setuptools
 
 setup(name='BrainMaGe',
-      version='1.0.0.Alpha',
+      version='1.0.0.RC2',
       description='Skull stripping using multiple and single modalities',
       url='https://github.com/CBICA/BrainMaGe',
       python_requires='>=3.6',
