b501047c7ef119db6afc86eebf81ec0cb94710bc
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-07-29 10:35:33 -0700

    Update benchmark-pt-ov.py

--- openvino/benchmark-pt-ov.py
+++ openvino/benchmark-pt-ov.py
@@ -18,17 +18,24 @@ from tqdm import tqdm
 import pandas as pd
 from pathlib import Path
 from timeit import default_timer as timer
-from datetime import timedelta
+from datetime import datetime
 
 from openvino.inference_engine import IECore
 
 brainmage_root = Path('../')
 
-nfbs_dataset_csv = 'nfbs-dataset.csv'
-nfbs_dataset_csv = 'nfbs-dataset-test.csv'
-nfbs_dataset_csv = 'nfbs-dataset-test-10.csv'
-nfbs_dataset_csv = 'nfbs-dataset-test-5.csv'
-#nfbs_dataset_csv = 'nfbs-dataset-test-1.csv'
+# dataset_csv = 'nfbs-dataset-test-1.csv'
+# dataset_csv = 'nfbs-dataset-preprocessed.csv'
+# dataset_csv = 'nfbs-dataset-test-preprocessed.csv'
+
+# dataset_csv = 'upenn-baseline-dataset.csv'
+# dataset_csv = 'upenn-baseline-dataset-test.csv'
+# dataset_csv = 'upenn-baseline-dataset-test-10.csv'
+dataset_csv = 'upenn-baseline-dataset-test-2.csv'
+
+sub_idx = 0 # 0th col is sub-id, 1st col is input path, 2nd col is mask_path
+input_path_idx = 3
+mask_path_idx = 2
 
 pt_output_path = 'pt-outfile' # PyTorch output file
 ov_output_path = 'ov-outfile' # ONNX output file
@@ -40,9 +47,10 @@ device="cpu"
 
 
 # ### Load Dataset csv
-
-nfbs_dataset_df = pd.read_csv(nfbs_dataset_csv, header = None)
-print("Number of rows:", nfbs_dataset_df.shape[0])
+dataset_df = pd.read_csv(dataset_csv, header = None)
+print(f"Number of rows: {dataset_df.shape[0]}")
+print(f"Input Image Sample: {dataset_df.iloc[0][input_path_idx]}")
+print(f"Mask Image Sample: {dataset_df.iloc[0][mask_path_idx]}")
 
 
 def bench_pytorch_fp32():
@@ -52,17 +60,17 @@ def bench_pytorch_fp32():
     pt_model.load_state_dict(checkpoint["model_state_dict"])
 
     ### Run PyTorch Inference
-    print (f"Starting PyTorch inference with {pytorch_model_path} ...")
+    print (f"\n Starting PyTorch inference with {pytorch_model_path} ...")
 
     _ = pt_model.eval()
 
     pt_stats =[]
 
     with torch.no_grad():
-        for i, row in tqdm(nfbs_dataset_df.iterrows()):
-            sub_id = row[0]
-            input_path = row[2]
-            mask_path = row[3]
+        for i, row in tqdm(dataset_df.iterrows()):
+            sub_id = row[sub_idx]
+            input_path = row[input_path_idx]
+            mask_path = row[mask_path_idx]
 
             try:
                 mask_image = get_mask_image(mask_path)
@@ -86,8 +94,13 @@ def bench_pytorch_fp32():
     print (f"Done PyTorch inference with {pytorch_model_path} ...")
     pt_stats_df = pd.DataFrame(pt_stats)
 
-    pt_stats_df.to_csv('pt_stats.csv', sep=',', header=False, index=False)
-    print (f"Saved pt_stats.csv ...")
+    date_time_str = datetime.now().strftime("%b-%d-%Y_%H-%M-%S")
+    csv_name = f"pt_stats_{date_time_str}.csv"
+    pt_stats_df.to_csv(csv_name, sep=',', header=False, index=False)
+    print (f"Saved {csv_name} ...")
+
+    print (f"\n PyTorch Dice Mean: {pt_stats_df[:][2].mean():.5f}")
+    print (f"PyTorch Total Inf Time: {pt_stats_df[:][3].sum():.2f} sec, Mean: {pt_stats_df[:][3].mean():.2f} sec")
 
     return pt_stats_df
 
@@ -114,10 +127,10 @@ def bench_ov_fp32():
 
     ov_stats =[]
 
-    for i, row in tqdm(nfbs_dataset_df.iterrows()):
-        sub_id = row[0]
-        input_path = row[2]
-        mask_path = row[3]
+    for i, row in tqdm(dataset_df.iterrows()):
+        sub_id = row[sub_idx]
+        input_path = row[input_path_idx]
+        mask_path = row[mask_path_idx]
 
         try:
             mask_image = get_mask_image(mask_path)
@@ -141,8 +154,13 @@ def bench_ov_fp32():
     print (f"Done OpenVINO inference with {ov_model_dir} ...")
     ov_stats_df = pd.DataFrame(ov_stats)
 
-    ov_stats_df.to_csv('ov_fp32_stats.csv', sep=',', header=False, index=False)
-    print (f"Saved ov_fp32_stats.csv ...")
+    date_time_str = datetime.now().strftime("%b-%d-%Y_%H-%M-%S")
+    csv_name = f"ov_fp32_stats_{date_time_str}.csv"
+    ov_stats_df.to_csv(csv_name, sep=',', header=False, index=False)
+    print (f"Saved {csv_name} ...")
+
+    print (f"\n OpenVINO FP32 Dice Mean: {ov_stats_df[:][2].mean():.5f}")
+    print (f"OpenVINO FP32 Total Inf Time: {ov_stats_df[:][3].sum():.2f} sec, Mean: {ov_stats_df[:][3].mean():.2f}")
 
     return ov_stats_df
 
@@ -170,10 +188,10 @@ def bench_ov_int8():
 
     ov_int8_stats =[]
 
-    for i, row in tqdm(nfbs_dataset_df.iterrows()):
-        sub_id = row[0]
-        input_path = row[2]
-        mask_path = row[3]
+    for i, row in tqdm(dataset_df.iterrows()):
+        sub_id = row[sub_idx]
+        input_path = row[input_path_idx]
+        mask_path = row[mask_path_idx]
 
         try:
             mask_image = get_mask_image(mask_path)
@@ -197,8 +215,13 @@ def bench_ov_int8():
     print (f"Done OpenVINO inference with {ov_model_dir} ...")
     ov_int8_stats_df = pd.DataFrame(ov_int8_stats)
 
-    ov_int8_stats_df.to_csv('ov_int8_stats.csv', sep=',', header=False, index=False)
-    print (f"Saved ov_int8_stats.csv ...")
+    date_time_str = datetime.now().strftime("%b-%d-%Y_%H-%M-%S")
+    csv_name = f"ov_int8_stats_{date_time_str}.csv"
+    ov_int8_stats_df.to_csv(csv_name, sep=',', header=False, index=False)
+    print (f"Saved {csv_name} ...")
+
+    print (f"\n OpenVINO INT8 Dice Mean: {ov_int8_stats_df[:][2].mean():.5f}")
+    print (f"OpenVINO INT8 Total Inf Time: {ov_int8_stats_df[:][3].sum():.2f}  sec, Mean: {ov_int8_stats_df[:][3].mean():.2f} sec")
 
     return ov_int8_stats_df
 
@@ -210,28 +233,36 @@ pt_stats_df = bench_pytorch_fp32()
 ov_stats_df = bench_ov_fp32()
 ov_int8_stats_df = bench_ov_int8()
 
+##
+## Print Summary
+##
+
+print(f"\n Mean Dice Scores for {dataset_df.shape[0]} images")
+print (f"PyTorch Dice Mean +/- STD: {pt_stats_df[:][2].mean():.5f} +/- {pt_stats_df[:][2].std():.1f}")
+print (f"OpenVINO FP32 Dice Mean +/- STD: {ov_stats_df[:][2].mean():.5f} +/- {ov_stats_df[:][2].std():.1f}")
+print (f"OpenVINO INT8 Dice Mean +/- STD: {ov_int8_stats_df[:][2].mean():.5f} +/- {ov_int8_stats_df[:][2].std():.1f}")
 
 dice_diff_pt_ov = pt_stats_df[:][2] - ov_stats_df[:][2]
 dice_diff_pt_ov_int8 = pt_stats_df[:][2] - ov_int8_stats_df[:][2]
 print()
-print(f"Accuracy Dice difference with OV FP32 {dice_diff_pt_ov.sum():.6f}")
+print(f"\n Accuracy Dice difference with OV FP32 {dice_diff_pt_ov.sum():.6f}")
 #print(dice_diff_pt_ov.value_counts())
-print(f"Accuracy Dice difference with OV INT8 {dice_diff_pt_ov_int8.sum():.6f}")
+print(f" Accuracy Dice difference with OV INT8 {dice_diff_pt_ov_int8.sum():.6f}")
 #print(dice_diff_pt_ov_int8.value_counts())
 
 pt_total_inf_time = pt_stats_df[:][3].sum()
 ov_total_inf_time = ov_stats_df[:][3].sum()
 ov_int8_total_inf_time = ov_int8_stats_df[:][3].sum()
 print()
-print(f"Total Inference Time (sec) for {pt_stats_df.shape[0]} images")
-print (f"PyTorch: {pt_total_inf_time:.2f} , Mean: {pt_stats_df[:][3].mean():.2f}")
-print (f"OpenVINO FP32: {ov_total_inf_time:.2f} , Mean: {ov_stats_df[:][3].mean():.2f}")
-print (f"OpenVINO INT8: {ov_int8_total_inf_time:.2f} , Mean: {ov_int8_stats_df[:][3].mean():.2f}")
+print(f"\n Total Inference Time (sec) for {dataset_df.shape[0]} images")
+print (f"PyTorch: {pt_total_inf_time:.2f} , Mean +/- STD: {pt_stats_df[:][3].mean():.2f} +/- {pt_stats_df[:][3].std():.2f} ")
+print (f"OpenVINO FP32: {ov_total_inf_time:.2f} , Mean +/- STD: {ov_stats_df[:][3].mean():.2f} +/- {ov_stats_df[:][3].std():.2f} ")
+print (f"OpenVINO INT8: {ov_int8_total_inf_time:.2f} , Mean +/- STD: {ov_int8_stats_df[:][3].mean():.2f} +/- {ov_int8_stats_df[:][3].std():.2f} ")
 
 speedup_fp32 = pt_total_inf_time/ov_total_inf_time
 speedup_int8 = pt_total_inf_time/ov_int8_total_inf_time
 speedup_fp32_int8 = ov_total_inf_time/ov_int8_total_inf_time
 print()
-print (f"Speedup with OpenVINO FP32 for {pt_stats_df.shape[0]} images: {speedup_fp32:.1f}x")
+print (f"\n Speedup with OpenVINO FP32 for {pt_stats_df.shape[0]} images: {speedup_fp32:.1f}x")
 print (f"Speedup with OpenVINO INT8 for {pt_stats_df.shape[0]} images: {speedup_int8:.1f}x")
 print (f"Speedup with OpenVINO INT8 over FP32: {speedup_fp32_int8:.1f}x")
