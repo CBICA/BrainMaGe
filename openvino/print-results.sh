19ba1b4982a939ec8db5279a0ce9f0bfd1b1114b
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-08-16 00:10:53 -0700

    Update print-results.sh

--- openvino/print-results.sh
+++ openvino/print-results.sh
@@ -1,2 +1,4 @@
 cat *.log | grep 'FP32: Total Inf Time \| FP32: Mean Inf \| ov_fp32'
 cat *.log | grep 'INT8: Total Inf Time \| INT8: Mean Inf \| ov_int8'
+
+cat *.log | grep -e 'PyTorch Total Inf Time*' -e 'PTFP32 Num Cores:'
