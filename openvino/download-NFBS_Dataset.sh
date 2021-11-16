2f5d2f077379079c2a31f30ca5e46c90c442393f
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2021-11-15 22:38:41 -0800

    Update download-NFBS_Dataset.sh

--- openvino/download-NFBS_Dataset.sh
+++ openvino/download-NFBS_Dataset.sh
@@ -1,5 +1,6 @@
 # Download NFBS_Dataset
 
 wget https://fcp-indi.s3.amazonaws.com/data/Projects/RocklandSample/NFBS_Dataset.tar.gz
+echo "Download complete. Extracting...."
 tar -xf NFBS_Dataset.tar.gz 
 
