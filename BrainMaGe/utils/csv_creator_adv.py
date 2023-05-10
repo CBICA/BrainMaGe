2fc6c5559042dfc5e5b822abf4ff3874174a90a6
Siddhesh Thakur <sid.cre8er@gmail.com>
2023-05-10 14:50:18 -0400

    Update rex_mul_csv

--- BrainMaGe/utils/csv_creator_adv.py
+++ BrainMaGe/utils/csv_creator_adv.py
@@ -101,60 +101,54 @@ def rex_sin_csv(folder_path, save_folder, file_type, modalities):
     print("CSV file saved successfully at:", csv_path)
 
 
-def rex_mul_csv(folder_path, to_save, ftype, modalities):
-    """[CSV generation for Multi Modalities]
-    [This function is used to generate a csv for multi mode and creates a csv]
-    Arguments:
-        folder_path {[string]} -- [Takes the folder to see where to look for
-                                   the different modaliies]
-        to_save {[string]} -- [Takes the folder as a string to save the csv]
-        ftype {[string]} -- [Are you trying to save train, validation or test,
-                             if file type is set to test, it does not look for
-                             ground truths]
-        modalities {[string]} -- [usually a string which looks like this
-                                  :  ['t1']]
+def rex_mul_csv(folder_path, save_folder, file_type, modalities):
+    """
+    CSV generation for Multi Modalities.
+    This function generates a csv for multi mode and creates a csv.
+
+    Args:
+        folder_path (str): The folder to see where to look for the different modalities
+        save_folder (str): The folder to save the csv
+        file_type (str): train, validation or test. If file_type is set to test, it does not look for ground truths.
+        modalities (list of str): The modalities to include in the csv.
     """
     modalities = modalities[1:-1]
     modalities = re.findall("[^, ']+", modalities)
     if not modalities:
-        print(
-            "Could not find modalities! Are you sure you have put in \
-              something in the modalities field?"
-        )
-        sys.exit(0)
-    if ftype == "test":
-        csv_file = open(os.path.join(to_save, ftype + ".csv"), "w+")
-        csv_file.write("ID,")
+        raise ValueError("Could not find modalities! Are you sure you have put in something in the modalities field?")
+        
+    if file_type == "test":
+        csv_path = os.path.join(save_folder, file_type + ".csv")
+        with open(csv_path, "w+") as csv_file:
+            csv_file.write("ID,")
     else:
-        csv_file = open(os.path.join(to_save, ftype + ".csv"), "w+")
-        csv_file.write("ID,gt_path,")
-    for modality in modalities[:-1]:
-        csv_file.write(modality + "_path,")
-    modality = modalities[-1]
-    csv_file.write(modality + "_path\n")
+        csv_path = os.path.join(save_folder, file_type + ".csv")
+        with open(csv_path, "w+") as csv_file:
+            csv_file.write("ID,gt_path,")
+            
+    csv_file.write(",".join([f"{modality}_path" for modality in modalities]))
+    csv_file.write("\n")
+    
     folders = os.listdir(folder_path)
     for folder in folders:
         csv_file.write(folder)
         csv_file.write(",")
-        if ftype != "test":
-            ground_truth = glob.glob(os.path.join(folder_path, folder, "*mask.nii.gz"))[
-                0
-            ]
-            csv_file.write(ground_truth)
+        
+        if file_type != "test":
+            ground_truth_path = glob.glob(os.path.join(folder_path, folder, "*mask.nii.gz"))[0]
+            csv_file.write(ground_truth_path)
             csv_file.write(",")
+            
         for modality in modalities[:-1]:
-            img = glob.glob(
-                os.path.join(folder_path, folder, "*" + modality + ".nii.gz")
-            )[0]
-            csv_file.write(img)
+            image_path = glob.glob(os.path.join(folder_path, folder, "*" + modality + ".nii.gz"))[0]
+            csv_file.write(image_path)
             csv_file.write(",")
-        modality = modalities[-1]
-        img = glob.glob(os.path.join(folder_path, folder, "*" + modality + ".nii.gz"))[
-            0
-        ]
-        csv_file.write(img)
+            
+        image_path = glob.glob(os.path.join(folder_path, folder, "*" + modalities[-1] + ".nii.gz"))[0]
+        csv_file.write(image_path)
         csv_file.write("\n")
-    csv_file.close()
+            
+    print("CSV file saved successfully at:", csv_path)
 
 
 def rex_bids_csv(folder_path, to_save, ftype):
